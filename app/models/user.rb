b8374e353448bf60b9ab37de052668bc27c536e5
綾乃 <>
2022-05-24 13:40:57 +0900

    rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { in: 2..20 }
   validates :email, presence: true
   validates :phone, presence: true
   validates :password, presence: true, length: { in: 6..10 }
-  
 end
