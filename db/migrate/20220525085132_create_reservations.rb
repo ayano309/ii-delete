38d092f6aeed257b7b2effb19346403c0a405bf3
綾乃 <>
2022-05-25 18:34:36 +0900

    reservationモデル作成

---
+++ db/migrate/20220525085132_create_reservations.rb
@@ -0,0 +1,12 @@
+class CreateReservations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reservations do |t|
+      t.integer :number_of_items, null: false
+      t.integer :visiting_time, null: false
+      t.integer :reservation_status, default: 0, null: false
+      t.references :user, foreign_key: true
+      t.references :capacity, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_25_071132) do
+ActiveRecord::Schema.define(version: 2022_05_25_085132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,18 @@ ActiveRecord::Schema.define(version: 2022_05_25_071132) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.integer "number_of_items", null: false
+    t.integer "visiting_time", null: false
+    t.integer "reservation_status", default: 0, null: false
+    t.bigint "user_id"
+    t.bigint "capacity_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["capacity_id"], name: "index_reservations_on_capacity_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -50,4 +62,6 @@ ActiveRecord::Schema.define(version: 2022_05_25_071132) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "capacities"
+  add_foreign_key "reservations", "users"
 end
